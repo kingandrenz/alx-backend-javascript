50dc36a74241da58e39e840d5e681f2af6dcccaf
kingandrenz <kanuchibueze@gmail.com>
2023-06-28 13:04:02 +0000

    task2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,14 @@
+const getNeighborhoodsList = () => {
+  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const addNeighborhood = (newNeighborhood) => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
+    return sanFranciscoNeighborhoods;
+  };
+
+  return {
+    addNeighborhood,
+  };
+};
+
+export default getNeighborhoodsList;
