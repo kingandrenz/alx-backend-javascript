ed4dd211265e3940c195d961c9f2bdb1fc8b5a0e
Anthony Kanu <kanuchibueze@gmail.com>
2023-07-08 01:36:21 +0000

    added new task

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,8 @@
-export default function TypedArrays(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-
-  const buffer = new ArrayBuffer(length);
-  const int8TypedArray = new Int8Array(buffer);
-  int8TypedArray[position] = value;
-  return buffer;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 }
